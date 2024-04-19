95fc1e7a6e396d46b59f37e001f47189759298fc
eli-johnson-04 <ed.j53190@gmail.com>
2024-04-19 13:34:40 -0400

    Commented sign-in-redirect.js

--- recipe-drop/src/components/sign-in-redirect.js
+++ recipe-drop/src/components/sign-in-redirect.js
@@ -4,9 +4,11 @@ import "@/app/globals.css";
 import { SIGN_IN_URL } from "@/routes";
 import { useRouter } from "next/navigation";
 
+// Define sign in button on website front page.
 const SignInRedirectButton = () => {
     const router = useRouter();
 
+    // Define button component, and route to sign in URL on click. 
     return (
         <button 
             className="ml-2 my-5 bg-transparent hover:bg-recipe-orange text-recipe-orange font-semibold hover:text-recipe-tan py-2 px-4 border border-recipe-orange hover:border-transparent rounded"
